92afad7392410abe1bbc092ba0fb2be33491dfde
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-12-26 09:09:24 -0300

    feat: registrando unit of work

--- src/Order.Infrastructure/InfraModule.cs
+++ src/Order.Infrastructure/InfraModule.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Order.Core.Repositories.Order.Core.Repositories;
 using Order.Infrastructure.Persistence.Repositories;
 
@@ -13,7 +14,14 @@ namespace Order.Infrastructure
 
         public static void AddRepositories(this IServiceCollection services)
         {
-            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUnitOfWork>(sp =>
+            {
+                var configuration = sp.GetRequiredService<IConfiguration>();
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                return connectionString is not null
+                ? new UnitOfWork(connectionString)
+                : throw new ArgumentNullException("The connection string 'DefaultConnection' is not configured");
+            });
         }
     }
 }
